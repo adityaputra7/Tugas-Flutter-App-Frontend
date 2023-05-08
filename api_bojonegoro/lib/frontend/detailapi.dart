f700172ec1d708a7159b8deeaf3b6bea8817beb8
Aditya Putra Irfandi <struv.gaming@gmail.com>
2023-05-09 00:58:55 +0700

    Initial commit

---
+++ api_bojonegoro/lib/frontend/detailapi.dart
@@ -0,0 +1,6 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:api_bojonegoro/model/kecamatanModels.dart';
+import 'package:api_bojonegoro/model/apiBojonegoro.dart';
+import 'package:api_bojonegoro/frontend/detailapi.dart';
